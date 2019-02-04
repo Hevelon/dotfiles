7b5e331148b9f31bdcb1ee04d57b9ece61f367fb
Hevelon <halisson.hevelon@gmail.com>
2019-02-04 10:34:09 -0300

    new file:   background/Dia.png 	new file:   background/Noite.png 	new file:   compton/compton.conf 	new file:   dunst/dunstrc 	new file:   i3/config 	new file:   neofetch/config.conf 	new file:   polybar/config 	new file:   rofi/config 	new file:   rofi/rofi.rasi 	new file:   scripts/archdex.sh 	new file:   scripts/background.sh 	new file:   scripts/bloqueio.sh 	new file:   scripts/menu.sh 	new file:   scripts/polybar.sh 	new file:   scripts/preparar.sh 	new file:   scripts/rpd-comercial.sh 	new file:   scripts/screencast.sh 	new file:   scripts/screenshot.sh 	new file:   scripts/spotify.sh 	new file:   sons/bip.wav 	new file:   termite/config

---
+++ scripts/bloqueio.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# www.jirrezdex.com
+# contato@jirrezdex.com
+# Jirrez Matheus
+
+i3lock-fancy -t 'Para desbloquear insira sua senha'
---
+++ scripts/preparar.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# www.jirrezdex.com
+# contato@jirrezdex.com
+# Jirrez Matheus
+
+pergunta() {
+    local prompt default reply
+    while true; do
+        if [ "${2:-}" = "Y" ]; then
+            prompt="S/N"
+            default=S
+        elif [ "${2:-}" = "N" ]; then
+            prompt="S/N"
+            default=N
+        else
+            prompt="S/N"
+            default=
+        fi
+        echo -n "$1 [$prompt] "
+        read reply </dev/tty
+        if [ -z "$reply" ]; then
+            reply=$default
+        fi
+        case "$reply" in
+            [Ss]*) return 0  ;;
+            [Nn]*) return  1 ;;
+        esac
+    done
+}
+
+echo -e "\e[44;90m== > Olá "$USER", Os arquivos estão sendo movidos para o diretório!\e[0m"
+    echo "< ================================================================= >";
+
+echo -e "\e[44;90m== > Diretório do repositório :: Acessado.\e[0m"
+    cd $HOME/dotfiles ;
+    git pull ;
+#    cd Configuração ;
+
+#echo -e "\e[44;90m== > Medidas protetivas :: Iniciadas.\e[0m"
+#    sudo rm -dR {background,compton,dunst,i3,neofetch,polybar,rofi,scripts,sons,termite} ;
+
+echo -e "\e[44;90m== > Arquivos :: Copiados.\e[0m"
+    cp -r $HOME/.config/{background,compton,dunst,i3,neofetch,polybar,rofi,scripts,sons} $HOME/dotfiles ;
+    sudo cp -r /etc/xdg/termite $HOME/dotfiles ;
+
+echo -e "\e[44;90m== > Preparação :: Concluída.\e[0m"
+    notify-send "Preparação concluída" ;
+    echo "< ================================================================= >";
+
+if pergunta "== > Fazer upload para o gitHub?" S; then
+    cd $HOME/dotfiles ;
+    git add --all ;
+    git commit ;
+    git push -u origin master ;
+    notify-send "Arquivos upados" ;
+fi
+
+canberra-gtk-play --file=$HOME/.config/sons/bip.wav
