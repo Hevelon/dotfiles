2b52c3b3b4d4886b517badb975b6d50733c86e02
Halisson <halisson.hevelon@gmail.com>
2019-02-05 00:56:18 -0300

    modified:   .bashrc 	new file:   background/Noite.jpg 	modified:   background/Noite.png 	new file:   background/Noite.png.copia 	modified:   i3/config 	modified:   scripts/background.sh

--- scripts/background.sh
+++ scripts/background.sh
@@ -6,11 +6,12 @@
 ################ Aplicando Wallpaper de acordo com a hora
 HORA=$(date "+%k")
     if ((HORA >= 20 )); then
-        feh --bg-scale $HOME/.config/background/Noite.png ;
+        feh --bg-tile $HOME/.config/background/Noite.jpg ;
+        #feh --bg-scale $HOME/.config/background/Noite.png ;
     elif ((HORA >= 6 )); then
-        feh --bg-scale $HOME/.config/background/Dia.png ;
+        feh --bg-tile $HOME/.config/background/Dia.png ;
     else
-        feh --bg-scale $HOME/.config/background/Noite.png ;
+        feh --bg-tile $HOME/.config/background/Noite.png ;
 fi 
 sleep 60m ;
 done
