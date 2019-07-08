fcfbeea6bc0afe3335816e14102497aae6e22c09
Halisson Hevelon <halisson.hevelon@gmail.com>
2019-07-08 10:12:13 -0300

    modified:   background/Dia.jpg 	modified:   background/Noite.png 	new file:   background/minimal-wallpapers-hd-1.jpg 	new file:   background/minimal-wallpapers-hd-2.jpg 	modified:   i3/config 	modified:   polybar/config 	modified:   scripts/background.sh

--- scripts/background.sh
+++ scripts/background.sh
@@ -6,12 +6,13 @@
 ################ Aplicando Wallpaper de acordo com a hora
 HORA=$(date "+%k")
     if ((HORA >= 20 )); then
-        feh --bg-tile $HOME/.config/background/Noite.png ;
-        #feh --bg-scale $HOME/.config/background/Noite.png ;
+        #feh --bg-tile $HOME/.config/background/Noite.png ;
+        feh --bg-scale $HOME/.config/background/Noite.png ;
     elif ((HORA >= 6 )); then
-        feh --bg-tile $HOME/.config/background/Dia.jpg ;
+        #feh --bg-tile $HOME/.config/background/Dia.jpg ;
+        DISPLAY=:0.0 feh --bg-scale $HOME/.config/background/Dia.jpg ;
     else
-        feh --bg-tile $HOME/.config/background/Noite.png ;
+        DISPLAY=:0.0 feh --bg-scale $HOME/.config/background/Noite.jpg ;
 fi 
 sleep 60m ;
 done
