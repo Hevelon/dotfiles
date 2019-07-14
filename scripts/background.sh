7496beb5fb8adc87b4a895985aeb8b2f3149a7b4
Halisson Hevelon <halisson.hevelon@gmail.com>
2019-07-14 00:30:32 -0300

    deleted:    background/minimal-wallpapers-hd-1.jpg 	deleted:    background/minimal-wallpapers-hd-2.jpg 	modified:   scripts/background.sh 	modified:   scripts/rpd-comercial.sh

--- scripts/background.sh
+++ scripts/background.sh
@@ -6,8 +6,8 @@
 ################ Aplicando Wallpaper de acordo com a hora
 HORA=$(date "+%k")
     if ((HORA >= 20 )); then
-        #feh --bg-tile $HOME/.config/background/Noite.png ;
-        feh --bg-scale $HOME/.config/background/Noite.png ;
+        feh --bg-tile $HOME/.config/background/Noite.png ;
+        #DISPLAY=:0.0 feh --bg-scale $HOME/.config/background/Noite.png ;
     elif ((HORA >= 6 )); then
         #feh --bg-tile $HOME/.config/background/Dia.jpg ;
         DISPLAY=:0.0 feh --bg-scale $HOME/.config/background/Dia.jpg ;
--- scripts/rpd-comercial.sh
+++ scripts/rpd-comercial.sh
@@ -6,14 +6,14 @@
 
 # Parâmetros do Rdesktop
 
-#		-a 16 -z -g 800x600
+#		-a 16 -z -a 16 -z -g 800x600
 # 		-f para tela cheia
-#		-g para definir resolução da tela 800x600, 1024x768, etc.
+#		-a 16 -z -g para definir resolução da tela 800x600, 1024x768, etc.
 #		-a para definir resolução da imagem em bits: 8, 16, 24
 #		-d domínio da rede utilizada
-#		-u nome do usuário a utilizar
+#		-5 -u nome do usuário a utilizar
 #		-p senha do usuário informado
-#		-k para o teclado: pt-br ou en-us
+#		-k para o teclado: pt-br ou en-5 -us
 #		-r redirecionamento de pasta
 #		-z = ativar compressão rdp
 # 		https://linux.die.net/man/1/rdesktop
@@ -29,15 +29,15 @@ if [ $resposta == 1 ]; then
 	echo "Digite (1) Comercial (2) João Nunes (3) ServerPDV (4) ServerERP: "
 	read escolha
 	if [ $escolha == 1 ]; then
-		rdesktop 192.168.1.103:33898 -g 1366x768 -PKD -k pt-br -5 -u Halisson -k pt-br -p Team152014 -r disk:Documents=/home/halisson/Pasta\ remota
+		rdesktop 192.168.1.103:33898 -a 16 -z -g 1366x768 -PKD -5 -u Halisson -k pt-br -p Team152014 -r disk:Documents=/home/halisson/Pasta\ remota
 
 	elif [ $escolha == 2 ]; then
-		rdesktop joaonunes.ddns.net -g 1366x768 -PKD -u Administrator -k pt-br -r disk:Documents=/home/halisson/Pasta\ remota
+		rdesktop joaonunes.ddns.net -a 16 -z -g 1366x768 -PKD -5 -u Administrator -k pt-br -r disk:Documents=/home/halisson/Pasta\ remota
 
 	elif [ $escolha == 3 ]; then
-		rdesktop 192.168.1.103:33898 -g 1366x768 -PKD -u Administrador -k pt-br -r disk:Documents=/home/halisson/Pasta\ remota
+		rdesktop 192.168.1.103:33898 -a 16 -z -g 1366x768 -PKD -5 -u Administrador -k pt-br -r disk:Documents=/home/halisson/Pasta\ remota
 	elif [ $escolha == 4 ]; then
-		rdesktop 192.168.1.118:3300 -g 1366x768 -PKD -u Administrator -k pt-br -r disk:Documents=/home/halisson/Pasta\ remota
+		rdesktop 192.168.1.118:3300 -a 16 -z -g 1366x768 -PKD -5 -u Administrator -k pt-br -r disk:Documents=/home/halisson/Pasta\ remota
 	else
 		echo "O valor fornecido é inválido!"
 	fi
@@ -48,10 +48,10 @@ elif [ $resposta == 2 ]; then
 
 	read escolha2
 	if [ $escolha2 == 1 ]; then
-		rdesktop antonioleno.ddns.net:33898 -g 1366x768 -PKD -u Halisson -k pt-br -p Team152014 -r disk:Documents=/home/halisson/Pasta\ remota
+		rdesktop antonioleno.ddns.net:33898 -a 16 -z -g 1366x768 -PKD -5 -u Halisson -k pt-br -r disk:Documents=/home/halisson/Pasta\ remota
 
 	elif [ $escolha2 == 2 ]; then
-		rdesktop joaonunes.ddns.net -g 1366x768 -PKD -u Administrator -k pt-br -r disk:Documents=/home/halisson/Pasta\ remota
+		rdesktop joaonunes.ddns.net -a 16 -z -g 1366x768 -PKD -5 -u Administrator -k pt-br -r disk:Documents=/home/halisson/Pasta\ remota
 	else
 		echo "O valor fornecido é inválido!"
 	fi
