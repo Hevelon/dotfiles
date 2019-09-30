05bd9e4f999765be90aa9c460f97d7e9c1b87713
Halisson Hevelon <halisson.hevelon@gmail.com>
2019-09-30 16:29:32 -0300

    modified:   .bashrc 	modified:   background/Dia.jpg 	modified:   background/Noite.png 	modified:   scripts/background.sh

--- .bashrc
+++ .bashrc
@@ -700,3 +700,6 @@ if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
--- scripts/background.sh
+++ scripts/background.sh
@@ -7,10 +7,10 @@
 HORA=$(date "+%k")
     if ((HORA >= 20 )); then
         feh --bg-tile $HOME/.config/background/Noite.png ;
-        #DISPLAY=:0.0 feh --bg-scale $HOME/.config/background/Noite.png ;
+
     elif ((HORA >= 6 )); then
-        #feh --bg-tile $HOME/.config/background/Dia.jpg ;
-        DISPLAY=:0.0 feh --bg-scale $HOME/.config/background/Dia.jpg ;
+        feh --randomize  --bg-tile $HOME/.config/background/Dia.jpg ;
+
     else
         DISPLAY=:0.0 feh --bg-scale $HOME/.config/background/Noite.jpg ;
 fi 
