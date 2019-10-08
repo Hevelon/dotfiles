8ea3e1af2ba36683d1f5e1634f0cee8fc291e24d
Halisson Hevelon <halisson.hevelon@gmail.com>
2019-10-08 09:14:59 -0300

    modified:   scripts/background.sh

--- scripts/background.sh
+++ scripts/background.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# www.jirrezdex.com
-# contato@jirrezdex.com
-# Jirrez Matheus
 
 ################ Aplicando Wallpaper de acordo com a hora
 HORA=$(date "+%k")
@@ -12,7 +9,7 @@ HORA=$(date "+%k")
         feh --randomize  --bg-tile $HOME/.config/background/Dia.jpg ;
 
     else
-        DISPLAY=:0.0 feh --bg-scale $HOME/.config/background/Noite.jpg ;
+        DISPLAY=:0.0 feh --bg-tile $HOME/.config/background/Noite.jpg ;
 fi 
 sleep 60m ;
 done
